3babbb705678147f5d8a357ef390567e01b43596
Richi <richiray15@gmail.com>
2026-08-20 16:25:50 +0530

    Revert "git push origin mainRevert "added cors""
    This reverts commit 31564fc367a6e20b242890caa0fa0f4b16f2250c.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'meals',
     'accounts',
     'config',
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,3 +154,4 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),  
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+CORS_ALLOW_ALL_ORIGINS = True 
